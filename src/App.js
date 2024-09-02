30d047c0f802ce6296072033d4749706d5af91c6
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-09-02 16:05:47 +0200

    css update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
+        <header className="App-header">The Dictionary</header>
         <main>
           <Dictionary />
         </main>
