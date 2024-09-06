c1f16dbd5b6264c479dbf383ab01461efeb00e76
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-09-06 15:24:43 +0200

    css styling added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">The Dictionary</header>
+        <header className="App-header"> 📖 The Dictionary</header>
         <main>
           <Dictionary defaultKeyword="dictionary" />
         </main>
-        <footer className="App-footer">Coded by Malgorzata Grajewska</footer>
+        <footer className="App-footer">
+          Coded by{" "}
+          <a
+            href="https://github.com/gosiagrajewska"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Małgorzata Grajewska
+          </a>{" "}
+          and open-sourced on{" "}
+          <a
+            href="https://github.com/gosiagrajewska/dictionaryapp-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
       </div>
     </div>
   );
