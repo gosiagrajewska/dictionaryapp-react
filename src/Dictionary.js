3fc1f63bbf636b0976dedee2c30a1d53fe2c28a1
gosiagrajewska <149269502+gosiagrajewska@users.noreply.github.com>
2024-09-09 10:55:13 +0200

    photos component added

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  function handleResponse(response) {
+  let [photos, setPhotos] = useState(null);
+
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handleImagesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let imagesApiKey = `tfc36afc870a6333078a74bc2fceao7b`;
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
 
   function handleSubmit(event) {
@@ -46,6 +57,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Synonyms.css";
 export default function Synonyms(props) {
-  console.log(props.synonyms);
   if (props.synonyms[0]) {
     return (
       <div className="synonyms">
